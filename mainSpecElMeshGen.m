3767657f618fddafb1c25b4c2f0fe4a9159cd67e
yildizdag <yildizdag@berkeley.edu>
2022-12-02 11:18:01 +0300

    first sample

--- mainSpecElMeshGen.m
+++ mainSpecElMeshGen.m
@@ -15,6 +15,36 @@ Nurbs2D = iga2Dmesh(FileName,numPatch,local_dof);
 iga2DmeshPlotNURBS(Nurbs2D);
 %---------------------------------------------------------------
 % Sampling Points for Spec. El. Method
-np_u = 3;
-np_v = 3;
-
+np_u = 5;
+np_v = 5;
+for k = 1:Nurbs2D.numpatch
+    for el = 1:Nurbs2D.nel{k}
+        iu = Nurbs2D.INC{k}(Nurbs2D.IEN{k}(1,el),1);   
+        iv = Nurbs2D.INC{k}(Nurbs2D.IEN{k}(1,el),2);
+        u_sample = linspace(Nurbs2D.knots.U{k}(iu),Nurbs2D.knots.U{k}(iu+1),np_u);
+        v_sample = linspace(Nurbs2D.knots.V{k}(iv),Nurbs2D.knots.V{k}(iv+1),np_v);
+        x_sample = zeros(np_u*np_v,1);
+        y_sample = zeros(np_u*np_v,1);
+        z_sample = zeros(np_u*np_v,1);
+        count = 1;
+        for j = 1:np_v
+            for r = 1:np_u
+                dNu = dersbasisfuns(iu,u_sample(r),Nurbs2D.order{k}(1)-1,0,Nurbs2D.knots.U{k});
+                dNv = dersbasisfuns(iv,v_sample(j),Nurbs2D.order{k}(2)-1,0,Nurbs2D.knots.V{k});
+                CP = Nurbs2D.cPoints{k}(:,iu-Nurbs2D.order{k}(1)+1:iu, iv-Nurbs2D.order{k}(2)+1:iv);
+                Sw = zeros(4,1);
+                for i = 1:4
+                    Sw(i,1) = dNu(1,:)*reshape(CP(i,:,:),Nurbs2D.order{k}(1),Nurbs2D.order{k}(2))*dNv(1,:)';
+                end
+                S = Sw(1:3,:) / Sw(4);
+                x_sample(count) = S(1);
+                y_sample(count) = S(2);
+                z_sample(count) = S(3);
+                count = count + 1;
+            end
+        end
+        hold on
+        scatter3(x_sample,y_sample,z_sample,'MarkerEdgeColor','k','MarkerFaceColor',[0 0 0]);
+        hold off
+    end
+end
